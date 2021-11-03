b2d1259ffa3285d46bc85405072b62a4b42c50a6
thinkverse <hallberg.kim@gmail.com>
2021-11-03 13:20:35 +0100

    Refactor routes

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,16 @@ Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->nam
 Route::middleware(['auth'])->group(function () {
     Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
     Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update'); 
+
+    // Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
+    // Add some middleware here, and change some code in app/Models/User.php to enable this
+    Route::view('/secretpage', 'secretpage')
+        ->name('secretpage')->middleware('verified');
+
+    // Task: this "/verysecretpage" URL should ask user for verifying their password once again
+    // You need to add some middleware here
+    Route::view('/verysecretpage', 'verysecretpage')
+        ->name('verysecretpage')->middleware('password.confirm');
 });
-
-// Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
-// Add some middleware here, and change some code in app/Models/User.php to enable this
-Route::view('/secretpage', 'secretpage')
-    ->name('secretpage')->middleware('verified');
-
-// Task: this "/verysecretpage" URL should ask user for verifying their password once again
-// You need to add some middleware here
-Route::view('/verysecretpage', 'verysecretpage')
-    ->name('verysecretpage')->middleware('password.confirm');
 
 require __DIR__.'/auth.php';
