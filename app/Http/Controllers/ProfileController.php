8b7ebc6a13f98041054694bd362b9121b9c103f7
Mukesh Tilokani <mtilokani@aecordigital.com>
2021-11-04 18:24:24 +0530

    Test Your Laravel Auth Skills

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Hash;
 use App\Http\Requests\ProfileUpdateRequest;
 
 class ProfileController extends Controller
@@ -15,6 +16,12 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        auth()->user()->update(array_filter([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => ($request->has('password')
+                ? Hash::make($request->password) : ''),
+        ]));
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
