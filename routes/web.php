3750b64178c00504d5aa5ccdad7eb559a2d9d734
Steve Chamaillard <steve@kang.fr>
2021-11-04 11:07:21 +0100

    Allow only users with verified email to access secretpage

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,7 @@ Route::get('users', [UserController::class, 'index'])->name('users.index');
 Route::get('profile', [ProfileController::class, 'show'])->middleware('auth')->name('profile.show');
 Route::put('profile', [ProfileController::class, 'update'])->middleware('auth')->name('profile.update');
 
-// Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
-// Add some middleware here, and change some code in app/Models/User.php to enable this
-Route::view('/secretpage', 'secretpage')
-     ->name('secretpage');
+Route::view('/secretpage', 'secretpage')->middleware('verified')->name('secretpage');
 
 // Task: this "/verysecretpage" URL should ask user for verifying their password once again
 // You need to add some middleware here
