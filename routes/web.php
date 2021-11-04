8a885aa85cdc086fef2106dd88b6dda35be5a2e2
Nathanaël Houn <contact@nathanaelhoun.fr>
2021-11-04 17:00:03 +0100

    5. Email verification

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::middleware('auth')->group(function () {
 // Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
 // Add some middleware here, and change some code in app/Models/User.php to enable this
 Route::view('/secretpage', 'secretpage')
+    ->middleware(\Illuminate\Auth\Middleware\EnsureEmailIsVerified::class)
     ->name('secretpage');
 
 // Task: this "/verysecretpage" URL should ask user for verifying their password once again
