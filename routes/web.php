c66d739d3b8a15d7301aba747143a26362fc3310
Steve Chamaillard <steve@kang.fr>
2021-11-04 10:52:36 +0100

    Cleanup code using FQN

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,20 +19,20 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+Route::get('users', [UserController::class, 'index'])->name('users.index');
 
 // Task: profile functionality should be available only for logged-in users
-Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
-Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+Route::get('profile', [ProfileController::class, 'show'])->name('profile.show');
+Route::put('profile', [ProfileController::class, 'update'])->name('profile.update');
 
 // Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
 // Add some middleware here, and change some code in app/Models/User.php to enable this
 Route::view('/secretpage', 'secretpage')
-    ->name('secretpage');
+     ->name('secretpage');
 
 // Task: this "/verysecretpage" URL should ask user for verifying their password once again
 // You need to add some middleware here
 Route::view('/verysecretpage', 'verysecretpage')
-    ->name('verysecretpage');
+     ->name('verysecretpage');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
