4fab703e40d6670988f53b158a3322ee036510ef
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2021-11-04 17:38:42 +0300

    Fix tests

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,18 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
     public function show()
     {
-        return view('auth.profile');
+        return view('auth.profile',['user' => auth()->user()]);
     }
 
     public function update(ProfileUpdateRequest $request)
     {
         // Task: fill in the code here to update name and email
+        auth()->user()->update($request->only(['name','email']));
+
         // Also, update the password if it is set
+        if ($request->has('password')){
+            auth()->user()->update(['password'=>Hash::make($request->password)]);
+        }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
