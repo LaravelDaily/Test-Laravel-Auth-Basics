b3820e70e4cd706db688c067dc52705b74ec0727
Maxie Wright <maxiewright@gmail.com>
2021-11-04 14:42:16 -0400

    Complete 7 Auth Skills Tasks

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -11,10 +13,17 @@ class ProfileController extends Controller
         return view('auth.profile');
     }
 
-    public function update(ProfileUpdateRequest $request)
+    public function update(ProfileUpdateRequest $request): RedirectResponse
     {
         // Task: fill in the code here to update name and email
+        $profileData = $request->validated();
+
         // Also, update the password if it is set
+        if ($request->has('password')){
+            $profileData['password'] = Hash::make($profileData['password']);
+        }
+
+        auth()->user()->update($profileData);
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
