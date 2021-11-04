131f2be73c23e409e16bd6a28ee523b10dce4d79
Meeraj Adhikari <atifmeeraj62@gmail.com>
2021-11-04 13:51:01 +0545

    Basic Auth Tests Passed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,13 +8,23 @@ class ProfileController extends Controller
 {
     public function show()
     {
+        // return auth()->user();
         return view('auth.profile');
     }
 
     public function update(ProfileUpdateRequest $request)
     {
         // Task: fill in the code here to update name and email
+                auth()->user()->update([
+            'name' => $request->name,
+            'email' => $request->email,
+        ]);
         // Also, update the password if it is set
+        if ($request->password) {
+            auth()->user()->update([
+                'password' => bcrypt($request->password),
+            ]);
+        }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
