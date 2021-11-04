29bb6b9b62bd90562087c60e9473325094ec23a5
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2021-11-04 19:37:34 +0300

    Avoid running 2 database requests on the same HTTP request

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,18 +9,21 @@ class ProfileController extends Controller
 {
     public function show()
     {
-        return view('auth.profile',['user' => auth()->user()]);
+        return view('auth.profile', ['user' => auth()->user()]);
     }
 
     public function update(ProfileUpdateRequest $request)
     {
         // Task: fill in the code here to update name and email
-        auth()->user()->update($request->only(['name','email']));
-
-        // Also, update the password if it is set
-        if ($request->has('password')){
-            auth()->user()->update(['password'=>Hash::make($request->password)]);
-        }
+        auth()->user()->update(
+            array_merge(
+                $request->only(['name', 'email']),
+                ($request->has('password')
+                    ? ['password' => Hash::make($request->password)]
+                    : []
+                )
+            )
+        );
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
