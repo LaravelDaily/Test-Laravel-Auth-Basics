0297439dc2bf31bb1f542e06c41065b545b52b4f
Steve Chamaillard <steve@kang.fr>
2021-11-04 11:01:34 +0100

    Update the password if it is set

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -13,8 +14,11 @@ class ProfileController extends Controller
 
     public function update(ProfileUpdateRequest $request)
     {
-        // Also, update the password if it is set
         auth()->user()->update($request->only('name', 'email'));
+
+        if ($request->has('password')) {
+            auth()->user()->update(['password' => Hash::make($request->password)]);
+        }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
