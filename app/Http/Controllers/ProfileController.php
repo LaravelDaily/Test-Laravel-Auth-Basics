48946496b6c13d5b027d3cf2223b3860f84af936
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2021-11-06 20:10:16 +0700

    All done
    I've learned a lot from this (and discover new things too), there'srules that actually exists but never being used before. And then middleware that actually already exist but just knowing today.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\{Auth, Hash};
 
 class ProfileController extends Controller
 {
@@ -15,6 +16,12 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $valid = $request->validated();
+        if ($request->password) {
+            $valid['password'] = Hash::make($valid['password']);
+        }
+
+        $request->user()->update($valid);
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
