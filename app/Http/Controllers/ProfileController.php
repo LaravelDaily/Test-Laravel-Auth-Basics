8d8ba5279509660a6256bdadafc081affd2b8256
Wilton Baltazar <wiltonbaltazar@gmail.com>
2021-11-11 16:05:17 +0200

    Auth Tasks completed 4

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -17,17 +17,17 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
-        $user = User::findOrFail(Auth::user()->id);
+        $user = auth()->user();
 
         $user->name = $request->name;
         $user->email =  $request->email;
 
         if ($request->has('password')) {
-            $user->password = bcrypt($request['password']);
+            $user->password = bcrypt($request->password);
         }
 
         $user->save();
-        
+
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
 }
