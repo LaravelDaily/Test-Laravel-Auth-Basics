ff03f86c69b0d2ab968d309214cb049828bed9b6
BeshoyFD <bishoy.eagle@yahoo.com>
2021-11-12 18:58:13 +0200

    tasks completed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,7 +14,7 @@ class ProfileController extends Controller
     public function update(ProfileUpdateRequest $request)
     {
         // Task: fill in the code here to update name and email
-        // Also, update the password if it is set
+        auth()->user()->update($request->validated());
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
