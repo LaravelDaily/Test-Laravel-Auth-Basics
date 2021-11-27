16cb4421a5fff833c670777cae712d4c558af194
Mohammed Jacksi <moh.jacksi@yahoo.com>
2021-11-27 11:39:49 +0300

    I just did the challange =)

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -1,4 +1,4 @@
-profile.blade.php<x-app-layout>
+<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('My profile') }}
