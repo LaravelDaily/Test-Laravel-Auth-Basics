7551cacbe607b0a29437a09138b7dbad89af77ff
Ivan Dabovic <ivan.dabovic88@yahoo.com>
2021-11-29 19:41:21 +0100

    Making all test go green.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -15,6 +16,22 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $this->validate($request, [
+            'name' => 'required|min:3|max:50',
+            'email' => 'email',
+            'password' => ['confirmed', 'min:6'],
+        ]);
+
+        $user = auth()->user();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        if ($request->password) {
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->save();
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
