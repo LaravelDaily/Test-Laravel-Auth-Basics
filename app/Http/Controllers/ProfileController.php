d2e63049845e9f279cbb7d33325d59af5d10f481
LAPTOP\raimonds.linde <raimonds.linde@gmail.com>
2021-12-14 17:51:21 +0200

    done

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,6 +15,15 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $validated = $request->validated();
+
+        if (isset($validated['password'])) {
+            $validated['password'] = bcrypt($validated['password']);
+        }
+        
+        $request->user()->update(
+            $validated
+        );
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
