bc4ce3c2e076915fda05d75828092a72d13d8ac6
Barbaro Javier <bjvalmaseda.g@gmail.com>
2021-12-30 10:51:48 -0500

    feat(auth): ✨ Task 5. Email Verification
    Make the URL /secretpage available only to those who verified their email. You need to make changes to two files.  In file routes/web.php add a Middleware to /secretpage URL. And enable email verification in the app/Models/User.php file.
    
    - Test method: test_email_can_be_verified()

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,37 +8,30 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+  use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var string[]
+   */
+  protected $fillable = ["name", "email", "password"];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array
+   */
+  protected $hidden = ["password", "remember_token"];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array
+   */
+  protected $casts = [
+    "email_verified_at" => "datetime",
+  ];
 }
