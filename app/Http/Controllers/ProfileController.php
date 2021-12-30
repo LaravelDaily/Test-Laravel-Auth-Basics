304add24a0777d4915f2977c63476f8b47b093e7
Barbaro Javier <bjvalmaseda.g@gmail.com>
2021-12-30 10:42:20 -0500

    feat(auth): ✨ Task 4. Profile Update.
    File app/Http/Controllers/ProfileController.php: fill in the method update() with the code to update the user's name and email. If the password is filled in, also update that
    
    Test methods:
    - test_profile_name_email_update_successful()
    - test_profile_password_update_successful()

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,19 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
-    public function show()
-    {
-        return view('auth.profile');
-    }
+  public function show()
+  {
+    return view("auth.profile");
+  }
 
-    public function update(ProfileUpdateRequest $request)
-    {
-        // Task: fill in the code here to update name and email
-        // Also, update the password if it is set
-
-        return redirect()->route('profile.show')->with('success', 'Profile updated.');
+  public function update(ProfileUpdateRequest $request)
+  {
+    if ($request->password) {
+      request()
+        ->user()
+        ->update([
+          "name" => $request->name,
+          "email" => $request->email,
+          "password" => Hash::make($request->password),
+        ]);
+    } else {
+      request()
+        ->user()
+        ->update([
+          "name" => $request->name,
+          "email" => $request->email,
+        ]);
     }
+    // Task: fill in the code here to update name and email
+    // Also, update the password if it is set
+
+    return redirect()
+      ->route("profile.show")
+      ->with("success", "Profile updated.");
+  }
 }
