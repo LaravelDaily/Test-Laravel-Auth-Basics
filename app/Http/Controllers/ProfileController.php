cf40601cfcb6e7751efdca799ba07306474bd0f8
MaS <mas@MaSs-MacBook-Pro.local>
2022-01-09 19:54:05 +0200

    Completed test

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\ProfileUpdateRequest;
 
 class ProfileController extends Controller
@@ -15,6 +16,18 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+
+        // Update name and email
+        auth()->user()->update([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+        ]);
+
+        if ($request->input('password')) {
+            auth()->user()->update([
+                'password' => Hash::make($request->input('password'),)
+            ]);
+        }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
