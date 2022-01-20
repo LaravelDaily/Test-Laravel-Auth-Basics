584eb79952b16297f4524aeb5311e535cb12f2f6
Awais Aslam <awaisaslam336@gmail.com>
2022-01-20 19:08:23 +0500

    Auth Test Solved

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,18 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
     public function show()
     {
-        return view('auth.profile');
+        $user = Auth::user();
+        return view('auth.profile',compact('user'));
     }
 
     public function update(ProfileUpdateRequest $request)
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $user = Auth::user();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        if($request->has('password')){
+            $user->password = Hash::make($request->password);
+        }
+        $user->save();
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
