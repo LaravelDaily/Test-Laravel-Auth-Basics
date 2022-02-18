fdc7aaa4762f72f22d62cf308dae6378a7624dc4
neo <neomn110@gmail.com>
2022-02-18 09:08:18 +0330

    all tasks done

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -15,6 +17,18 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+
+
+        $password = !empty($request->password) ? $request->password : null;
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email
+        ];
+
+        if (!empty($request->password))
+            $data['password']= Hash::make($request->password);
+
+        User::where( 'email' ,$request->email)->update($data);
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
