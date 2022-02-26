21954ac2c925f213ea1e34aad7956d91baa983a0
GianfrancoRocco <gianfrancorocco.business@gmail.com>
2022-02-25 22:22:28 -0300

    Passed

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileUpdateRequest extends FormRequest
 {
@@ -27,5 +28,20 @@ class ProfileUpdateRequest extends FormRequest
         if ($this->password == null) {
             $this->request->remove('password');
         }
+    }
+
+    protected function passedValidation()
+    {
+        if ($this->has('password')) {
+            $this->merge(['password' => Hash::make($this->password)]);
+        }
+    }
+
+    public function validated(): array
+    {
+        if ($this->has('password')) {
+            return array_merge(parent::validated(), ['password' => $this->input('password')]);
+        }
+        return parent::validated();
     }
 }
