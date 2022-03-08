801ee5c763c8dcae9e9771d5da9a582bf246684c
Rudolph Sta. Maria <razez0444@gmail.com>
2022-03-08 16:49:43 +0800

    All auth tests done

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\ModelNotFoundException as ME;
+use DB;
+use Validator;
 
 class ProfileController extends Controller
 {
@@ -16,6 +22,33 @@ class ProfileController extends Controller
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
 
-        return redirect()->route('profile.show')->with('success', 'Profile updated.');
+        DB::beginTransaction();
+
+        try {
+            $user = Auth::user();
+                
+                $user->name = $request->name;
+                $user->email = $request->email;
+
+                if (isset($request->password)) {
+                    $user->update([
+                        "password" => Hash::make($request->password),
+                    ]);
+                }
+                $user->update();
+                DB::commit();
+        } catch (Exception $e) {
+            DB::rollback();
+            return response()->json(
+                [
+                    'errors' => ['Something went wrong'],
+                ],
+                500
+            );
+        }
+
+        return redirect()
+            ->route('profile.show')
+            ->with('success', 'Profile updated.');
     }
 }
