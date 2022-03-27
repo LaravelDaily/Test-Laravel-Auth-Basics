8740909712d2e44d3b171e201e8c8cb0f6c2c6db
nikita <gorbacs.nikita@gmail.com>
2022-03-28 01:00:02 +0300

    Third commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
 use App\Models\User;
+use Exception;
 
 class ProfileController extends Controller
 {
@@ -16,18 +17,24 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
-        if ($request->has('password')) {
-            User::update([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password)
-            ]);
-        }
 
-        User::update([
-            'name' => $request->name,
-            'email' => $request->email
-        ]);
+        try {
+            $user = Auth::user();
+            $user->name = $request->name;
+            $user->email = $request->email;
+
+            if ($request->has('password')) {
+                $user->password = Hash::make($request->password);
+            }
+            $user->update();
+        } catch (Exception $error) {
+            return response()->json(
+                [
+                    'error' => 'Something went wrong'
+                ],
+                500
+            );
+        }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
