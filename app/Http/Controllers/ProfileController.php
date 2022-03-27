972453201bab82ef8a34de493ef8882ae9ce60d1
nikita <gorbacs.nikita@gmail.com>
2022-03-28 01:48:54 +0300

    Test

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Exception;
 
 class ProfileController extends Controller
