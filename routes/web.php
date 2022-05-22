01bf4bac8a122aec0ab6891ee88b3ae826a2aee5
Md. Abir Chowdhury <40237786+YouKnowMe2@users.noreply.github.com>
2022-05-22 17:00:34 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['middleware'=>'auth'],function(){
+
+
+// Task: profile functionality should be available only for logged-in users
+    Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
+    Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+
+
+});
 Route::get('/', function () {
     return view('home');
 })->name('home');
 
 Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
-
-// Task: profile functionality should be available only for logged-in users
-Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
-Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
-
 // Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
 // Add some middleware here, and change some code in app/Models/User.php to enable this
 Route::view('/secretpage', 'secretpage')
@@ -32,5 +36,7 @@ Route::view('/secretpage', 'secretpage')
 // You need to add some middleware here
 Route::view('/verysecretpage', 'verysecretpage')
     ->name('verysecretpage');
+
+
 
 require __DIR__.'/auth.php';
