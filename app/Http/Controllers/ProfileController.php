3b49f063238a0a3082b127a22133204a774446ba
omda <eabdulwahid@wallpostdev.com>
2022-05-23 15:26:53 +0200

    laravel auth test solution

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
-
+use App\Models\User;
+use Auth;
 class ProfileController extends Controller
 {
     public function show()
@@ -15,7 +16,12 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
-
+        $user = Auth::user();
+        $password = isset($request->password) ? bcrypt($request->password):$user->password;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $password;
+        $user->save();
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
 }
