052c26fe3c3b81403d186b38a66aeff7cc884a77
ahmed fayez <ahmed.fayez.ayesh@gmail.com>
2022-05-24 00:07:00 +0200

    Laravel Auth - Pass All Tests Successfully

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\ProfileUpdateRequest;
 
 class ProfileController extends Controller
@@ -15,6 +17,12 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        auth()->user()->update($request->validated());
+        if ($request->has('password')) {
+            User::whereId(auth()->user()->id)->update([
+                'password' => Hash::make($request->password)
+            ]);
+        }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
