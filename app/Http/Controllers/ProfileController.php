1875bebbdfe100f7f81ae5d13d3b2fa64c17d0db
Md. Abir Chowdhury <40237786+YouKnowMe2@users.noreply.github.com>
2022-05-24 23:02:00 +0600

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,6 +15,22 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+          $request->validate([
+            'name' => [ 'string', 'max:255'],
+            'email' => [ 'string', 'email', 'max:255'],
+            'password' => [ 'confirmed', Password::min(8)->letters()],
+        ]);
+        $user =User::findOrFail(Auth::id());
+
+        auth()->user()->update($request->validated());
+
+        if($request->has('password')){
+            $user->password = Hash::make($request->password);
+        }
+        $user->save();
+
+
+
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
