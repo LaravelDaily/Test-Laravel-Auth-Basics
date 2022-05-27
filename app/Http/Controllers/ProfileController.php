8367f336f11d483a9594325c5dc814f15185d79f
wongandy <wong_andy31@yahoo.com>
2022-05-27 14:10:25 +0000

    finished all tasks

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -16,6 +16,16 @@ class ProfileController extends Controller
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
 
+        if ($request->has('password')) {
+            $request->user()->update([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password)
+            ]);
+        } 
+        else {
+            $request->user()->update($request->only('name', 'email'));
+        }
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
 }
