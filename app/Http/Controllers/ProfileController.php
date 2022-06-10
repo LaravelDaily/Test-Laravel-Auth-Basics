43d8c618e4ec9cbfdd42a1b5408eabbbd452acd7
avanserij <a.serij12@gmail.com>
2022-06-10 13:10:20 +0300

    New Auth Test

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
-
+use Illuminate\Support\Facades\Auth;
 class ProfileController extends Controller
 {
     public function show()
@@ -15,6 +15,15 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+
+        $user = Auth::user();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        if ($request->has('password'))
+            $user->password = bcrypt($request->input('password'));
+
+        $user->save();
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
