ca46619485a06ec37b3b6ef98b3ba49d32fe0301
SVFCode <svfcode@mail.ru>
2022-06-24 19:15:19 +0500

    Done

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,7 +14,19 @@ class ProfileController extends Controller
     public function update(ProfileUpdateRequest $request)
     {
         // Task: fill in the code here to update name and email
+        $data = $request->validated();
+
+        $user = auth()->user();
+
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+
         // Also, update the password if it is set
+        if(isset($data['password'])) {
+            $user->password = bcrypt($data['password']);
+        }
+
+        $user->save();
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
