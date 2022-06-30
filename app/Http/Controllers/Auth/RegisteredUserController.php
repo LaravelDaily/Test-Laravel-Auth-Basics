527e96a14550777b23f5940c63b9374a12255ff3
edoorinaldoo@gmail.com <edoorinaldoo@gmail.com>
2022-07-01 02:14:47 +0700

    update

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
+use Illuminate\Auth\Events\Registered;
+use App\Providers\RouteServiceProvider;
 
 class RegisteredUserController extends Controller
 {
--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\ProfileUpdateRequest;
-use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
@@ -15,15 +17,15 @@ class ProfileController extends Controller
     public function update(ProfileUpdateRequest $request)
     {
         // Task: fill in the code here to update name and email
-        auth()->user()->update($request->validated());
+        
         // Also, update the password if it is set
 
+        auth()->user()->update($request->validated());
         if ($request->has('password')) {
             User::whereId(auth()->user()->id)->update([
                 'password' => Hash::make($request->password)
             ]);
         }
-
 
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
