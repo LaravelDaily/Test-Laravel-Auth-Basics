9af2b2eacac1c6936d4f1b3caa2b316d92be3b15
Ali Ilyes MANSOUR <mansour.aliilyes@gmail.com>
2022-07-08 12:35:28 +0100

    Challenge Completed

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,7 @@
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
                         {{ __('Users') }}
                     </x-nav-link>
-                    {{-- Task: this "Profile" link should be visible only to logged-in users --}}
+                    {{-- Task: this "Profile" link should be visible only to logged-in userss --}}
                     @auth
                         <x-nav-link href="/profile" :active="request()->routeIs('profile.show')">
                             {{ __('Profile') }}
