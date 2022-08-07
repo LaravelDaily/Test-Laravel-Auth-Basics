8fce89649ba7e77eafed8599e073e008cf69c4bd
Gardi <ahmadgardi0@gmail.com>
2022-08-07 15:56:30 +0300

    Task 6 & 7 Complete

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,8 +13,19 @@ class ProfileController extends Controller
 
     public function update(ProfileUpdateRequest $request)
     {
-        // Task: fill in the code here to update name and email
-        // Also, update the password if it is set
+       if($request->password != null){
+              $request->user()->update([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password)
+              ]);
+       }
+       else {
+                $request->user()->update([
+                    'name' => $request->name,
+                    'email' => $request->email
+                ]);
+       }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
