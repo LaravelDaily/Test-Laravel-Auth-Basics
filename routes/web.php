6d0f4d1840f9daea0e5d3dcce6c3b1a0dcad33c5
cur1ousFranz <xfranjeff@gmail.com>
2022-09-04 14:58:54 +0800

    auth basics

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::middleware('auth')->group(function (){
 Route::view('/secretpage', 'secretpage')
     ->name('secretpage')->middleware('verified');
     
+    
 // Task: this "/verysecretpage" URL should ask user for verifying their password once again
 // You need to add some middleware here
 Route::view('/verysecretpage', 'verysecretpage')
