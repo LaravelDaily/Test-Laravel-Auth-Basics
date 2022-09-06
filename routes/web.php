43205fcae2069a87225a60e30ab356e6995dfad5
MuhammedMukdad <muhammednmukdad1999m@gmail.com>
2022-09-06 12:38:04 -0700

    solve1

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
 
-Route::middleware('auth')->group(function{
+Route::middleware('auth')->group(function(){
 // Task: profile functionality should be available only for logged-in users
 Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
 Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
