f7340fbb33555300e6af2c1ff60d2637c1b02e4b
MuhammedMukdad <muhammednmukdad1999m@gmail.com>
2022-09-06 12:35:33 -0700

    solve

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,6 +15,13 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $user = auth()->user();
+        $user->email = $request->email;
+        $user->name = $request->name;
+        if ($request->has('password')) {
+            $user->password = Hash::make($request->password);
+        }
+        $user->save();
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
