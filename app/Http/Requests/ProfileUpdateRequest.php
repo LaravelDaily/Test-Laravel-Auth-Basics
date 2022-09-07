9c97fe30ecdb11866e4ed4106e5b6cde5a64ad23
sohail Ahmad <sohailahmad0757@gmail.com>
2022-09-07 18:57:10 +0500

    All Test Passed

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -1,31 +1,31 @@
 <?php
-
-namespace App\Http\Requests;
-
-use Illuminate\Validation\Rule;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Foundation\Http\FormRequest;
-
-class ProfileUpdateRequest extends FormRequest
-{
+  
+  namespace App\Http\Requests;
+  
+  use Illuminate\Foundation\Http\FormRequest;
+  use Illuminate\Support\Facades\Auth;
+  use Illuminate\Validation\Rule;
+  
+  class ProfileUpdateRequest extends FormRequest
+  {
     public function rules()
     {
-        return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'email', 'string', 'max:255', Rule::unique('users')->ignore(Auth::user())],
-            'password' => ['sometimes', 'required_with:old_password', 'string', 'confirmed', 'min:8'],
-        ];
+      return [
+        'name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'email', 'string', 'max:255', Rule::unique('users')->ignore(Auth::user())],
+        'password' => ['sometimes', 'required_with:old_password', 'string', 'confirmed', 'min:8'],
+      ];
     }
-
+    
     public function authorize()
     {
-        return true;
+      return true;
     }
-
+    
     protected function prepareForValidation()
     {
-        if ($this->password == null) {
-            $this->request->remove('password');
-        }
+      if ($this->password == null) {
+        $this->request->remove('password');
+      }
     }
-}
+  }
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,44 +1,44 @@
 <?php
-
-namespace App\Models;
-
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-
-class User extends Authenticatable
-{
+  
+  namespace App\Models;
+  
+  use Illuminate\Contracts\Auth\MustVerifyEmail;
+  use Illuminate\Database\Eloquent\Factories\HasFactory;
+  use Illuminate\Foundation\Auth\User as Authenticatable;
+  use Illuminate\Notifications\Notifiable;
+  use Laravel\Sanctum\HasApiTokens;
+  
+  class User extends Authenticatable implements MustVerifyEmail
+  {
     use HasApiTokens, HasFactory, Notifiable;
-
+    
     /**
      * The attributes that are mass assignable.
      *
      * @var string[]
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+      'name',
+      'email',
+      'password',
     ];
-
+    
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+      'password',
+      'remember_token',
     ];
-
+    
     /**
      * The attributes that should be cast.
      *
      * @var array
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+      'email_verified_at' => 'datetime',
     ];
-}
+  }
