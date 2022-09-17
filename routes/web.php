619f97f309505b27a7d50fe67dc195fd90502a2d
Robert Gonçalves Batista <robert.comunicar@gmail.com>
2022-09-16 23:51:42 -0300

    [16-09-2022] - Os testes passaram

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::view('/secretpage', 'secretpage')
     ->middleware(['verified']);
 
 // Task: this "/verysecretpage" URL should ask user for verifying their password once again
-// You need to add some middleware here
+// You need to add some middleware here 
 Route::view('/verysecretpage', 'verysecretpage')
     ->name('verysecretpage')
     ->middleware(["password.confirm"]);
