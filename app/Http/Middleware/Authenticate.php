b71abdcd9023be78f577f41f74ee7e8e250cee45
Wassim_Ammar <mohamedwassim.ammar@etudiant-isi.utm.tn>
2022-10-03 12:02:43 +0100

    Solved Challenge

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,10 +12,13 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
+
+     
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
             return route('login');
         }
     }
+    
 }
