c247040938a8aa2b721680623d8f9356f82db61f
Khaled Ahmed <36667919+KhaledTolba@users.noreply.github.com>
2022-10-09 00:20:56 +0200

    update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 use App\Http\Requests\ProfileUpdateRequest;
 
 class ProfileController extends Controller
@@ -14,8 +16,15 @@ class ProfileController extends Controller
     public function update(ProfileUpdateRequest $request)
     {
         // Task: fill in the code here to update name and email
+        $formFields = [
+            'name' => $request->name,
+            'email' => $request->email
+        ];
         // Also, update the password if it is set
-
+        if ($request->has('password')){
+            $formFields['password'] = Hash::make($request->input('password'));
+        }
+        auth()->user()->update($formFields);
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
 }
