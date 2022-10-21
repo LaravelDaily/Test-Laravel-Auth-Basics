c1d522a806ed384507932f35bc188f61d1ba9ae8
IlijaAngelov <ilija.angelov@yahoo.com>
2022-10-21 21:04:47 +0200

    Regex for Password Field

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -37,7 +37,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'regex:/^\d*[a-zA-Z][a-zA-Z0-9]*$/', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
