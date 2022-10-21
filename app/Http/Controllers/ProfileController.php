cc8f194d56fb0c15ed12cc57981eb07256ae0c8e
IlijaAngelov <ilija.angelov@yahoo.com>
2022-10-21 20:14:42 +0200

    Test Auth Basics

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\ProfileUpdateRequest;
 
 class ProfileController extends Controller
@@ -15,6 +17,15 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $form = $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email']
+        ]);
+
+        if(isset($request['password'])){
+            $form['password'] = Hash::make($request['password']);
+        }
+        auth()->user()->update($form);
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -23,13 +23,12 @@
                             <br /><br />
 
                             <x-label for="name" :value="__('Name')"/>
-
                             {{-- Task: replace ??? for name/email with logged in user's name/email --}}
                             <x-input id="name"
                                      class="block mt-1 w-full"
                                      type="text"
                                      name="name"
-                                     value="???"
+                                     value="{{ Auth::user()->name; }}"
                                      required />
                         </div>
 
@@ -40,7 +39,7 @@
                                      class="block mt-1 w-full"
                                      type="email"
                                      name="email"
-                                     value="???"
+                                     value="{{ Auth::user()->email; }}"
                                      required />
                         </div>
 
