06592025e1f23616db551969d7f2b8381d7c553b
chrisng99 <christopher.nwh99@gmail.com>
2022-11-14 21:48:04 +0800

    Check for answers

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,6 +15,12 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        auth()->user()->update($request->only('name', 'email'));
+        if ($request->input('password')) {
+            auth()->user()->update([
+                'password' => bcrypt($request->password)
+            ]);
+        }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
