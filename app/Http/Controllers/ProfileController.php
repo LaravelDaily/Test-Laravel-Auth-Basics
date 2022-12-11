a04b98bd128ab2df1553752b05b7453aa978d331
george <george@bpoc.co.jp>
2022-12-11 22:11:33 +0800

    updated auth test functionality

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -14,7 +15,17 @@ class ProfileController extends Controller
     public function update(ProfileUpdateRequest $request)
     {
         // Task: fill in the code here to update name and email
+        $data =    $request->validate([
+            'name'  =>  'required',
+            'email' =>  'required'
+        ]);
+
         // Also, update the password if it is set
+        if($request->has('password')){
+            $data['password']   =   Hash::make($request->input('password'));
+        }
+
+        $request->user()->update($data);
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
