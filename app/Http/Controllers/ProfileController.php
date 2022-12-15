3f81c8718bc0e33e6409c502c6d6883a4a18191f
Mohammad Morakabati <mhmmdtech@yahoo.com>
2022-12-15 17:20:11 +0330

    solve auth challenge

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -15,7 +17,12 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
-
+        $inputs = $request->only('name', 'email');
+        $user = $request->user();
+        if ($request->has('password')) {
+            $inputs['password'] = Hash::make($request->password);
+        }
+        $user->update($inputs);
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
 }
