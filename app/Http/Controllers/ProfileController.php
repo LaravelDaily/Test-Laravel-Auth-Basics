78dc0a959fb173949ec586a01e926cbc928839bd
erikbocil <erikluis3006@gmail.com>
2022-12-19 21:08:55 +0700

    Exercise

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -16,6 +16,7 @@ class ProfileController extends Controller
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
         $validated = $request->validated();
+        dd();
         $request->user()->update($validated);
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
