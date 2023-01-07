d018c79e4434197307525938143d948e607592fb
Muhammad Arnaldo <muhammad.arnaldo@gmail.com>
2023-01-07 09:46:46 +0700

    Tasks

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,6 +15,19 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $user = Auth::user();
+        if ($request->has('password')) {
+            $user->update([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => Hash::make($request->input('password'))
+            ]);
+        } else {
+            $user->update([
+                'name' => $request->input('name'),
+                'email' => $request->input('email')
+            ]);
+        }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
