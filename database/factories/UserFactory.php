9611be8ebc49f4b0a03e0c53e6465e2f0f829602
Emre Erden <e.erden@iqdh.eu>
2023-01-18 14:27:08 +0100

    changed hashed password in UserFactory to plain text password cause of eloquent mutators in User Model, it will hash the given password automatically on create or update. implemented auth skill tasks

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,11 +22,12 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        //'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => 'password', // password
             'remember_token' => Str::random(10),
         ];
     }
