830664282c6ccc4fd8ff35ea45fc050835439a0b
mshoubasi <mr8wolf@gmail.com>
2023-01-19 07:54:53 +0200

    auth-test-success

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -37,7 +37,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['regex:([a-zA-z][0-9])', 'required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
