5e9944a59302e073233158bad09d9a552e3ca354
Abdelrhman Abouelez <Abouelez10@gmail.com>
2023-02-08 14:19:50 +0200

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
@@ -15,7 +17,6 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
-
         $validated = $request->validated();
 
         $user = auth()->user();
