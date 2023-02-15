80a16d030324565df2758c4fd756b6638e26a3e8
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-02-15 14:19:44 +0500

    profile functionality added only for logged-in users

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,11 +19,13 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+Route::get('users', [UserController::class, 'index'])->name('users.index');
 
 // Task: profile functionality should be available only for logged-in users
-Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
-Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('profile', [ProfileController::class, 'show'])->name('profile.show');
+    Route::put('profile', [ProfileController::class, 'update'])->name('profile.update');
+});
 
 // Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
 // Add some middleware here, and change some code in app/Models/User.php to enable this
@@ -33,4 +37,4 @@ Route::view('/secretpage', 'secretpage')
 Route::view('/verysecretpage', 'verysecretpage')
     ->name('verysecretpage');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
