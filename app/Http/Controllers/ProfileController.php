f28320ee323fbfab7cd62996a723a00262eb0337
MekadAlibrahem <mekadalibrahem123@gmail.com>
2023-02-19 03:07:38 +0300

    test 01  task all

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -18,10 +18,12 @@ class ProfileController extends Controller
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
         $user = Auth::user();
-        $user->name = $request->name ;
-        $user->email =$request->email ;
-        $request->has('password') ? $user->password = Hash::make($request->password) : '' ;
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $request->has('password') ? $user->password = Hash::make($request->password) : '';
         $user->save();
+        
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
