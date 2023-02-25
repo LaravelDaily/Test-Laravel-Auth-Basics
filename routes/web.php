c14f68e99ed85aa5c6fbdb4c0338833f149d8e7d
Yeraldi Mujica <94128962+Yeraldi29@users.noreply.github.com>
2023-02-25 09:42:30 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,16 @@ Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update']
 
 // Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
 // Add some middleware here, and change some code in app/Models/User.php to enable this
-Route::view('/secretpage', 'secretpage')
-    ->name('secretpage')
-    ->middleware(['auth','verified']);
+Route::middleware('verified')->group(function () {
+    Route::view('/secretpage', 'secretpage')
+        ->name('secretpage');
+});
 
 // Task: this "/verysecretpage" URL should ask user for verifying their password once again
 // You need to add some middleware here
-Route::view('/verysecretpage', 'verysecretpage')
-    ->name('verysecretpage')
-    ->middleware(['password.confirm']);
+Route::middleware('password.confirm')->group(function () {
+    Route::view('/verysecretpage', 'verysecretpage')
+        ->name('verysecretpage');
+});
 
 require __DIR__.'/auth.php';
