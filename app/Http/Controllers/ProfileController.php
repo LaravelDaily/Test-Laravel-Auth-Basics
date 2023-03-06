aa0746df8b7996f6e5609d47f819a15363e3ad07
Daniel Montes <Daniel Montes>
2023-03-06 06:39:28 +0000

    all tests passing

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -15,6 +16,14 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+
+        $data = $request->validated();
+
+        if(isset($data['password'])){
+            $data['password'] = Hash::make($data['password']);
+        }
+
+        auth()->user()->update($data);
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
