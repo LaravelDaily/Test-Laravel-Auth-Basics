7564e6ab010f3802fc8f496b19e46d4896635910
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-03-14 20:52:02 +0100

    Rest of the tasks ✅

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,16 +6,27 @@ use App\Http\Requests\ProfileUpdateRequest;
 
 class ProfileController extends Controller
 {
-    public function show()
-    {
-        return view('auth.profile');
-    }
+	public function show()
+	{
+		return view('auth.profile');
+	}
 
-    public function update(ProfileUpdateRequest $request)
-    {
-        // Task: fill in the code here to update name and email
-        // Also, update the password if it is set
+	public function update(ProfileUpdateRequest $request)
+	{
+		// Task: fill in the code here to update name and email
+		// Also, update the password if it is set
 
-        return redirect()->route('profile.show')->with('success', 'Profile updated.');
-    }
+		$user = auth()->user();
+
+		$user->name = $request['name'];
+		$user->email = $request['email'];
+
+		if ($request['password']) {
+			$user->password = bcrypt($request['password']);
+		}
+
+		$user->save();
+
+		return redirect()->route('profile.show')->with('success', 'Profile updated.');
+	}
 }
