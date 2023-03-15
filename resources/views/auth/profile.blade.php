7c843529a2460dda98d53bfeb747f946eeae6251
Toheeb Oyekola <Toheeb.Olawale.TO23@gmail.com>
2023-03-15 06:52:53 +0100

    task 1 to 4 completed

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -29,7 +29,9 @@
                                      class="block mt-1 w-full"
                                      type="text"
                                      name="name"
-                                     value="???"
+                                     value="
+                                     @auth {{ auth()->user->name }} @endauth 
+                                     @guest {{ ??? }} @endguest"
                                      required />
                         </div>
 
@@ -40,7 +42,9 @@
                                      class="block mt-1 w-full"
                                      type="email"
                                      name="email"
-                                     value="???"
+                                     value="
+                                     @auth {{ auth()->user->email }} @endauth 
+                                     @guest {{ ??? }} @endguest"
                                      required />
                         </div>
 
