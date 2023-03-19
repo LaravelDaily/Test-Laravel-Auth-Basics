9ceb54cf33c3e99a07ebe315037be6e093138f25
Muhammed Abdelshafy <57871519+Xafy@users.noreply.github.com>
2023-03-19 02:07:50 +0000

    fix email verification

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::middleware('auth')->group(function (){
 // Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
 // Add some middleware here, and change some code in app/Models/User.php to enable this
 Route::view('/secretpage', 'secretpage')
-    ->middleware(['auth', 'verified'])
+    ->middleware('verified')
     ->name('secretpage');
 
 // Task: this "/verysecretpage" URL should ask user for verifying their password once again
