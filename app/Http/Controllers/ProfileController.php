a71a061293c181748bffb823981f4eb54c94a7ae
alloyking1 <alloyking1@gmail.com>
2023-04-15 20:54:33 +0100

    5 test case passed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class ProfileController extends Controller
 {
@@ -15,6 +19,16 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+
+        if (!empty($request->password)) {
+            Auth::user()->update([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
+        } else {
+            $update =  Auth::user()->update($request->only(['name', 'email']));
+        }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
