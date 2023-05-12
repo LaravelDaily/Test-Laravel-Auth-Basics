d26c189b9f128a78cff67f3506684af2d6e969ac
lucidlynxx <dzakysyahrizal04@gmail.com>
2023-05-12 11:16:32 +0700

    eighth test

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -38,7 +38,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'alpha_num:ascii', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'ascii', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
