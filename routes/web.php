46e848f8286f1852759169b50c31ea366f4f1490
sothylor <panha.saloeun@gmail.com>
2023-05-28 08:46:51 +0700

    Task #5

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::put('profile', [ProfileController::class, 'update'])->middleware('auth')-
 // Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
 // Add some middleware here, and change some code in app/Models/User.php to enable this
 Route::view('/secretpage', 'secretpage')
-    ->name('secretpage');
+    ->name('secretpage')->middleware(['verified']);
 
 // Task: this "/verysecretpage" URL should ask user for verifying their password once again
 // You need to add some middleware here
