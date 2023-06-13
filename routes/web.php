b75dcd330fc1e3b5b7e3db591e395eb0bc465a96
Hector <hmreluz@gmail.com>
2023-06-13 14:40:27 -0500

    Test email can be verified

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,10 @@ Route::group(['middleware' => ['auth']], function () {
 
 // Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
 // Add some middleware here, and change some code in app/Models/User.php to enable this
-Route::view('/secretpage', 'secretpage')
-    ->name('secretpage');
+Route::group(['middleware' => ['verified']], function () {
+    Route::view('/secretpage', 'secretpage')
+        ->name('secretpage');
+});
 
 // Task: this "/verysecretpage" URL should ask user for verifying their password once again
 // You need to add some middleware here
