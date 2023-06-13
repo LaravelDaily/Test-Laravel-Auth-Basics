fa3392389f3b0bbb17cd99df6a746c0e76bda20a
Hector <hmreluz@gmail.com>
2023-06-13 14:48:37 -0500

    Test password confirmation page

--- routes/web.php
+++ routes/web.php
@@ -27,14 +27,12 @@ Route::group(['middleware' => ['auth']], function () {
 
 // Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
 // Add some middleware here, and change some code in app/Models/User.php to enable this
-Route::group(['middleware' => ['verified']], function () {
-    Route::view('/secretpage', 'secretpage')
-        ->name('secretpage');
-});
+Route::view('/secretpage', 'secretpage')
+    ->name('secretpage')->middleware('verified');
 
 // Task: this "/verysecretpage" URL should ask user for verifying their password once again
 // You need to add some middleware here
 Route::view('/verysecretpage', 'verysecretpage')
-    ->name('verysecretpage');
+    ->name('verysecretpage')->middleware('password.confirm');
 
 require __DIR__.'/auth.php';
