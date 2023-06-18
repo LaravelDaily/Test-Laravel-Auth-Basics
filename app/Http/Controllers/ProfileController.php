86a11eb9d48104cd882f71de36c80875020a375b
Osanga Yusuf <yusufjenom@gmail.com>
2023-06-18 19:45:09 +0100

    Tasks completed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
@@ -15,6 +16,15 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $user = Auth::user();
+
+        $new_profile = $request->validated();
+
+        if ($request->validated('password')) {
+            $new_profile['password'] = bcrypt($request->validated('password'));
+        }
+
+        $user->update($new_profile);
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
