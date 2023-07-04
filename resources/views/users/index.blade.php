fab20b36df507fad39f524cefe59d44eebcb7f78
abdullah6516 <61629432+abdullah6516@users.noreply.github.com>
2023-07-04 17:03:00 +0300

    [fixes]

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,24 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('Users') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-                    <div class="overflow-hidden overflow-x-auto min-w-full align-middle sm:rounded-md">
-                        <table class="min-w-full divide-y divide-gray-200 border">
+                    <div class="min-w-full overflow-hidden overflow-x-auto align-middle sm:rounded-md">
+                        <table class="min-w-full border divide-y divide-gray-200">
                             <thead>
                             <tr>
                                 <th class="px-6 py-3 bg-gray-50">
-                                    <span class="text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Name</span>
+                                    <span class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Name</span>
                                 </th>
                                 <th class="px-6 py-3 bg-gray-50">
-                                    <span class="text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Email</span>
+                                    <span class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Email</span>
                                 </th>
                             </tr>
                             </thead>
@@ -26,10 +26,10 @@
                             <tbody class="bg-white divide-y divide-gray-200 divide-solid">
                             @foreach($users as $user)
                                 <tr class="bg-white">
-                                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                    <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
                                         {{ $user->name }}
                                     </td>
-                                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                    <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
                                         {{ $user->email }}
                                     </td>
                                 </tr>
