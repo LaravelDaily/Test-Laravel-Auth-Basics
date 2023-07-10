bd75d22cac670920496488ea658430881d40db6f
Hussain Mayoof <hf.mayoof@gmail.com>
2023-07-10 12:54:35 +0300

    Commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -16,6 +16,27 @@ class ProfileController extends Controller
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
 
+        $user = auth()->user();
+
+        $incomingFields = $request->validate([
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'min:8'
+        ]);
+
+        $incomingFields['name'] = strip_tags($incomingFields['name']);
+        $incomingFields['email'] = strip_tags($incomingFields['email']);
+
+        $user->name = $incomingFields['name'];
+        $user->email = $incomingFields['email'];
+
+        if (isset($incomingFields['password'])) {
+            $incomingFields['password'] = bcrypt(strip_tags($incomingFields['password']));
+            $user->password = $incomingFields['password'];
+        }
+
+        $user->save();
+
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
 }
