6b7384ad4bf91f9fc2b96bb17d15f8d20ce862ed
datasandwich <jarekettl98@gmail.com>
2023-07-14 11:56:58 +0100

    ui

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Auth;
 
 
 class ProfileController extends Controller
@@ -17,8 +18,8 @@ class ProfileController extends Controller
         
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
-        
-        $user = auth()->user;
+
+        $user = Auth::user();
 
         $new_profile = $request->validated();
 
