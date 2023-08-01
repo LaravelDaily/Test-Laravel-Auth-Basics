3bb04c4e5f5719b53a1327152246e4334a18a997
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-01 13:13:23 +0330

    task six

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::view('/secretpage', 'secretpage')
 // Task: this "/verysecretpage" URL should ask user for verifying their password once again
 // You need to add some middleware here
 Route::view('/verysecretpage', 'verysecretpage')
+    ->middleware(['password.confirm'])
     ->name('verysecretpage');
 
 require __DIR__.'/auth.php';
