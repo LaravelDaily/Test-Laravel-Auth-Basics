83517640b29a31381d623a5d827ebe1970830782
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-01 17:28:46 +0330

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -22,7 +22,7 @@ class ProfileController extends Controller
             'email' => $request->email
                       ]);
         if (isset($request->password))
-            $user->update([ 'password' => Hash::make($request->email) )];
+            $user->update([ 'password' => Hash::make($request->email) ]);
         
         
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
