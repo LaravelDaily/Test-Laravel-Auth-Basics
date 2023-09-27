5e80d4fa2a13dbcb6bafbfedaeb87af8d5a6df81
MZMohamed <zubair85@gmail.com>
2023-09-27 15:56:15 +0200

    wip

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,6 +15,16 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $name = $request->input('name');
+        $email = $request->input('email');
+
+        if($request->input('password')) {
+            // update pass
+        }
+
+        
+
+        
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
