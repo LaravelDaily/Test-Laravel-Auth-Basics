7d4a541673ecaa23a1db4a6dd061e76a6916e249
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-11-13 15:12:37 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password'
+        'password',
+    'email_verified_at'
     ];
 
     /**
