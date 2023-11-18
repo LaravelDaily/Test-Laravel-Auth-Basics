57c5ce85351e189b15732b46e47fc367cf4f3682
ammar <ammar@fmail.com>
2023-11-18 19:04:07 +0200

    1

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,14 @@ Route::get('/', function () {
 
 Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
 
+Route::middleware('auth')->group(function (){
+
 // Task: profile functionality should be available only for logged-in users
 Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
 Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
 
+
+Route::middleware('auth')->group(function (){
 // Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
 // Add some middleware here, and change some code in app/Models/User.php to enable this
 Route::view('/secretpage', 'secretpage')
@@ -32,5 +36,7 @@ Route::view('/secretpage', 'secretpage')
 // You need to add some middleware here
 Route::view('/verysecretpage', 'verysecretpage')
     ->name('verysecretpage');
+});
+});
 
 require __DIR__.'/auth.php';
