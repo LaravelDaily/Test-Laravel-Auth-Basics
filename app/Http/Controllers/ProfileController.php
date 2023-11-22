142bb76cb996df6222dcf35617971ddfcc0ba914
Alexandr_Illarionov <illarionovbook@gmail.com>
2023-11-22 07:59:50 +0100

    Test-Laravel-Auth-Basics; done

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
@@ -15,6 +16,13 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $validData = $request->validated();
+
+        if (isset($validData['password'])) {
+            $validData['password'] = bcrypt($validData['password']);
+        }
+
+        Auth::user()->update($validData);
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
