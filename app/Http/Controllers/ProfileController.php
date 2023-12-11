14a1b4713fa3a7090b6d57ddffdb3f08a48acb3e
geldibayev <geldibayevbega@gmail.com>
2023-12-11 10:02:46 +0500

    fourth task has done

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,6 +15,16 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $user = auth()->user();
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+        ]);
+
+        if ($request->filled('password')) {
+            $user->password = bcrypt($request->input('password'));
+        }
+        $user->save();
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
