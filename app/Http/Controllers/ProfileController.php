2a3cfd0bafc7e6ffc4917b0344e4111908fec704
Ahmad Essam <72272968+ahmadessam5@users.noreply.github.com>
2024-01-29 10:25:05 +0200

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -18,7 +18,7 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
-        $user = Auth::user;
+        $user = $request->$user();
         $user->update($request->safe()->only(['name','email']));
 
         if($request->filled('password')){
