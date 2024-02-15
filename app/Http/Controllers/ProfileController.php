c5a0b91c900255a1f22a52cf6c81f25f15142355
Virtue Egerega <egeregav@gmail.com>
2024-02-15 09:59:16 -0800

    Testing Auth skills in laravel

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,8 +13,26 @@ class ProfileController extends Controller
 
     public function update(ProfileUpdateRequest $request)
     {
-        // Task: fill in the code here to update name and email
-        // Also, update the password if it is set
+
+        $validated = $request->validate([
+            'current_password' => ['bail', 'nullable', 'current_password'],
+            'password' => ['bail', 'nullable', Rules\Password::numbers()->mixedCase(), 'confirmed'],
+        ]);
+
+        $request->user()->fill($request->validated());
+
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        $request->user()->save();
+
+        if($request->has('password')){
+            $request->user()->update([
+                'password' => Hash::make($validated['password']),
+            ]);
+        }
+        
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
