20ca042de20f700f048aae9e627ac34a355ad3c1
Xuan <108394451+xuanxuan27@users.noreply.github.com>
2024-02-18 21:12:11 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * verified email
+     */
+    public function hasVerifiedEmail()
+    {
+        return !is_null($this->email_verified_at);
+    }
 }
