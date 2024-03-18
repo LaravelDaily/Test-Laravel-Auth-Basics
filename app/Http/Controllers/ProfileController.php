fa70cb7aaea892ba683f1008eabb00bb61af79fa
Railton98 <railtonleal98@gmail.com>
2024-03-18 16:12:17 -0300

    fix test: test_profile_name_email_update_successful

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
@@ -13,8 +14,12 @@ class ProfileController extends Controller
 
     public function update(ProfileUpdateRequest $request)
     {
-        // Task: fill in the code here to update name and email
-        // Also, update the password if it is set
+        /** @var User $user */
+        $user = auth()->user();
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        $user->save();
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
