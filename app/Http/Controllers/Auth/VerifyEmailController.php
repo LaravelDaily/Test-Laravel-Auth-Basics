0966696cfa3479085c6506964c8b0e483bc55a8c
Ranie <143940800+ranyesantos@users.noreply.github.com>
2024-03-22 19:54:03 -0300

    auth features working

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -19,7 +19,7 @@ class VerifyEmailController extends Controller
     {
         if ($request->user()->hasVerifiedEmail()) {
             return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
-        }
+        } 
 
         if ($request->user()->markEmailAsVerified()) {
             event(new Verified($request->user()));
--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -16,6 +16,16 @@ class ProfileController extends Controller
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
 
+        $user = auth()->user();
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        if ($request->password){
+            $user->password = bcrypt($request->password);
+        };
+
+        $user->save();
+        
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
 }
